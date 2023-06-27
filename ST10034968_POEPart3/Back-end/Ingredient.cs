baf01cb30354437d2711e25f6945167398a4473f
Gabriella Janssen <ST10034968@vcconnect.edu.za>
2023-06-27 15:02:31 +0200

    initial commit

---
+++ ST10034968_POEPart3/Back-end/Ingredient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: ST10034968
+ */
+
+namespace ST10034968_POEPart1
+{
+    public class Ingredient
+    {
+        //declarations
+        //properties
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string RUnit { get; set; }
+        public double Quantity { get; set; }
+        public double RQuantity { get; set; } = 0;
+        public double Calories { get; set; }
+        public string FoodGroup { get; set; }
+        
+
+        //constructors
+        public Ingredient(string name, string unitOfMeasurement, double quantity, double calories, string foodGroup)
+        {
+            this.Name = name;
+            this.Unit = unitOfMeasurement;
+            this.Quantity = quantity;
+            this.Calories = calories;
+            FoodGroup = foodGroup;
+        }
+        //default constructor
+        public Ingredient()
+        {
+            this.Name = "DefaultName";
+            this.Unit = "DefaultUnit";
+            this.Quantity = 0;
+        }
+
+        //print number of ingredients
+        public string printIngredient()
+        {
+            string output;
+            output = $"{Quantity} {Unit} of {Name}\t\tCalories: {Calories},\tFood group: {FoodGroup}";
+
+            if(RQuantity > 0)
+            {
+                output = $"{Quantity} {Unit} and {RQuantity} {RUnit} of {Name}\t\tCalories: {Calories},\tFood group: {FoodGroup}";
+            }
+            return output;
+        }
+
+        //methods to convert units
+        //method converts units up to a larger unit
+        //Imperial unit conversions done according to https://keeshaskitchen.com/kitchen-conversion-charts/
+        public void convertUp()
+        {
+            bool end = false;
+            //detecting what type of unit measurement is the ingredient and converting accordingly
+            while(end == false)
+            {
+                if (Unit.Equals("tablespoons") && Quantity >= 16)
+                {
+                    conversion("tablespoons", "cups", 16);
+                }
+                else if (Unit.Equals("teaspoons") && Quantity >= 3)
+                {
+                    conversion("teaspoons", "tablespoons", 3);
+                }
+                else if (Unit.Equals("ounces") && Quantity >= 8)
+                {
+                    conversion("ounces", "cups", 8);
+                }
+                else if (Unit.Equals("grams") && Quantity >= 1000)
+                {
+                    conversion("grams", "kilograms", 1000);
+                }
+                else if (Unit.Equals("millilitres") && Quantity >= 1000)
+                {
+                    conversion("millilitres", "litres", 1000);
+                }
+                else
+                {
+                    end = true;
+                }
+            }
+            
+        }
+        //method converts units down to smaller units
+        public void convertDown()
+        {
+            bool end = false;
+            //detecting what type of unit measurement is the ingredient and converting accordingly
+            while (end == false && Quantity < 1)
+            {
+                if (Unit.Equals("cups"))
+                {
+                    Quantity = 16 * Quantity + RQuantity;
+                    Unit = "tablespoons";
+                    RUnit = " ";
+
+                    RQuantity = Quantity - Math.Round(Quantity);
+                    Quantity -= RQuantity;
+                    if (RQuantity > 0)
+                    {
+                        RUnit = "teaspoons";
+                        RQuantity *= 3;
+                    }
+                }
+                else if (Unit.Equals("tablespoons"))
+                {
+                    Quantity = 3 * Quantity + RQuantity;
+                    Unit = "teaspoons";
+                    RUnit = " ";
+                    RQuantity = 0;
+                }
+                else if (Unit.Equals("kilograms"))
+                {
+                    Quantity = 1000 * Quantity + RQuantity;
+                    Unit = "grams";
+                    RUnit = " ";
+                    RQuantity = 0;
+                }
+                else if (Unit.Equals("litres"))
+                {
+                    Quantity *= 1000 * Quantity + RQuantity;
+                    Unit = "millilitres";
+                    RUnit = " ";
+                    RQuantity = 0;
+                }
+                else
+                {
+                    end = true;
+                }
+            }
+        }
+        //following method is called within convertUp() method
+        public void conversion(string unit, string cUnit, double divisor) 
+        {
+            RQuantity = Quantity % divisor;
+            RUnit = unit;
+            Quantity = Math.Round(Quantity / divisor);
+            Unit = cUnit;
+        }
+    }
+}
---
+++ ST10034968_POEPart3/Back-end/Recipe.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+/*
+ * Author: ST10034968
+ */
+
+namespace ST10034968_POEPart1
+{
+    public class Recipe
+    {
+        //declarations
+        public string name;
+        public List<string> steps;
+        public List<Ingredient> ingredients;
+        public List<Ingredient> oldIngredients;
+        public bool quantitiesAltered = false;
+        public double totalCalories = 0;
+        //delegate for event
+        public event EventHandler<double> OnCalorieOver300;
+
+        //Constructor
+        public Recipe(string nameEntered, List<string> stepsEntered, List<Ingredient> ingredientsEntered)
+            {
+                this.name = nameEntered;
+                this.steps = stepsEntered;
+                this.ingredients = ingredientsEntered;
+                oldIngredients = new List<Ingredient>();
+                //calculate total calories
+                calcTotalCalories();
+            }
+        //default constructor
+        public Recipe()
+        {
+        }
+
+        //methods
+        //method that scales the recipe by designated factors
+        public void scale(double factor)
+        {
+            if (quantitiesAltered == false)
+            {
+                for (int i = 0; i < ingredients.Count; i++)
+                {
+                    oldIngredients.Add(makeCopy(ingredients.ElementAt(i)));
+                }
+ 
+            }
+
+            for (int i = 0; i < ingredients.Count; i++)
+            {
+                ingredients.ElementAt(i).Quantity *= factor;
+                ingredients.ElementAt(i).RQuantity *= factor;
+                ingredients.ElementAt(i).Calories *= factor;
+                ingredients.ElementAt(i).convertUp();
+                ingredients.ElementAt(i).convertDown();
+            }
+
+            calcTotalCalories();
+
+            quantitiesAltered = true;
+            
+        }
+        
+        //method to reset previosly scaled values to original measurements
+        public void reset()
+        {
+            //clearing ingredients list
+            ingredients.Clear();
+            //copying data from old ingredients list to ingredients list
+            for (int i = 0; i < oldIngredients.Count; i++)
+            {
+                ingredients.Add(makeCopy(oldIngredients.ElementAt(i)));
+            }
+            //clearing old ingredients list
+            oldIngredients.Clear();
+
+            quantitiesAltered = false;
+        }
+        //overriding toString method 
+        public override string ToString()
+        {
+            string line = "==========================================================================";
+            string output = ($"{line}\n" +
+                $"{this.name}\n" +
+                $"{line}\n");
+
+            //printing ingredients and numbered steps
+            output += ("Ingredients:\n");
+            for (int i = 0; i < this.ingredients.Count; i++)
+            {
+                output += (this.ingredients[i].printIngredient() + "\n");
+            }
+            output += ("Steps:\n");
+            for (int i = 0; i < this.steps.Count; i++)
+            {
+                output += ($"Step {i + 1}:\n" +
+                    $"{this.steps[i]}\n");
+            }
+
+            output += ($"Total calories: {this.calcTotalCalories()}\n");
+
+            // values for recommended amount of calories were found at
+            // https://www.nhs.uk/live-well/healthy-weight/managing-your-weight/understanding-calories/
+            if (totalCalories > 2500)
+            {
+                output += ("Total calories exceed the recommended daily calories for an adult male.\n");
+            }
+            else if (totalCalories > 2000)
+            {
+                output += ("Total calories exceed the recommended daily calories for an adult female.\n");
+            }
+            else if (totalCalories > 1250)
+            {
+                output += ("Total calories exceed half the recommended daily calories for an adult male.\n");
+            }
+            else if (totalCalories > 1000)
+            {
+                output += ("Total calories exceed half the recommended daily calories for an adult female.\n");
+            }
+
+            output += ($"The recommended amount of calories for an adult male:\t2500 calories\n" +
+                $"The recommended amount of calories for an adult female:\t2000 calories\n" +
+                $"However many other factors such as age and exercise can change these recommended daily calory amounts\n");
+            output += ($"{line}\n" +
+                $"{line}\n");
+
+            return output;
+        }
+        //method to calculate total calories which invokes event if total calories are over 300
+        public double calcTotalCalories()
+        {
+            double totalCal = 0;
+            foreach (Ingredient ing in ingredients)
+            {
+                totalCal += ing.Calories;
+            }
+            this.totalCalories = totalCal;
+            //invoking event if calories are over 300
+            if (totalCal > 300)
+            {
+                this.OnCalorieOver300?.Invoke(this,this.totalCalories);
+            }
+            return totalCal;
+        }
+        //method that returns a copy of an ingredient inputted
+        public Ingredient makeCopy(Ingredient original)
+        {
+            //individually hard copies each property and variable of original ingredient
+            string name = original.Name;
+            string unit = original.Unit;
+            string rUnit = original.RUnit;
+            double quantity = original.Quantity;
+            double rQuantity = original.RQuantity;
+            double calories = original.Calories;
+            string foodGroup = original.FoodGroup;
+
+            Ingredient copy = new Ingredient(name, unit, quantity, calories, foodGroup);
+            copy.RUnit = rUnit;
+            copy.RQuantity = rQuantity;
+
+            return copy;
+        }
+    }
+}
